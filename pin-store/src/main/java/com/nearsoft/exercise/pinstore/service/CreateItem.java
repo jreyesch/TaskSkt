97a147763cdf247d5852f3be615fefbf57054027
Jorge Reyes <jorgereyes@NSL-JREYESs-MacBook-Pro.local>
2018-05-28 10:23:07 -0700

    Removed MongoDB image from docker-compose.yml Added CreateItem interface to pin-store Added ListItem interface to pin-store Added error.html file to pin-store Configured RabbitMQ port on application.yml

---
+++ pin-store/src/main/java/com/nearsoft/exercise/pinstore/service/CreateItem.java
@@ -0,0 +1,8 @@
+package com.nearsoft.exercise.pinstore.service;
+
+import com.nearsoft.exercise.pinstore.model.Item;
+
+interface CreateItem {
+
+    public boolean createSingleItem(Item item);
+}
---
+++ pin-store/src/main/java/com/nearsoft/exercise/pinstore/service/ListItem.java
@@ -0,0 +1,9 @@
+package com.nearsoft.exercise.pinstore.service;
+
+import com.nearsoft.exercise.pinstore.model.Item;
+import java.util.List;
+
+interface ListItem {
+
+    public List<Item> findaAll();
+}
